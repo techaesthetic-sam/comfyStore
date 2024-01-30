21432d96cda24a00781869c15c1de5dfdabb268b
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-01-30 12:00:31 +0530

    init and pages created

---
+++ my-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+function App() {
+  return (
+    <>
+      <About />
+    </>
+  );
+}
+
+export default App;
---
+++ my-project/src/pages/About.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <h1 className="text-4xl">About</h1>;
+}
