7ded2a2d11c1711c1ca8106852486dc51c516a93
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-01-30 16:16:42 +0530

    implemented V6.4

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
   About,
   Cart,
@@ -12,12 +13,56 @@ import {
   Register,
   SingleProduct,
 } from "./pages";
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <About />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- my-project/src/pages/HomeLayout.jsx
+++ my-project/src/pages/HomeLayout.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { Outlet } from "react-router-dom";
 export default function HomeLayout() {
-  return <div>HomeLayout</div>;
+  return (
+    <nav>
+      <span className="text-3xl font-bold">Comfy Sloth </span>
+      <Outlet />
+    </nav>
+  );
 }
