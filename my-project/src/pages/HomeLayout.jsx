aaf08cd897297d1787e471f02d520e1c957145f1
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-01-31 13:13:07 +0530

    about navbar navLinks

--- my-project/src/pages/HomeLayout.jsx
+++ my-project/src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components";
+import { Header, Navbar } from "../components";
 export default function HomeLayout() {
   return (
     <nav>
       <Header />
+      <Navbar />
       <section className="align-element py-8">
         <Outlet />
       </section>
