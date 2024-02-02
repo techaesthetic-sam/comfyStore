16db9558f92fb3b184762f150413bc4d9f7bf351
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-02-02 21:18:31 +0530

    forminput/ products

--- my-project/src/components/Filters.jsx
+++ my-project/src/components/Filters.jsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { Form, useLoaderData, Link } from "react-router-dom";
+import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
+const Filters = () => {
+  const { meta } = useLoaderData();
+  return (
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
+      />
 
-export default function Filters() {
-  return <div>Filters</div>;
-}
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["a-z", "z-a", "high", "low"]}
+        size="select-sm"
+      />
+      {/* BUTTONS */}
+      <button type="submit" className="btn btn-primary btn-sm ">
+        search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        reset
+      </Link>
+    </Form>
+  );
+};
+export default Filters;
